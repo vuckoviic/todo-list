9ecf8aab0dcd5cccf6b3bf1edf9feab91ee6c814
vuckoviic <vuckovica64@gmail.com>
2024-02-22 11:32:02 +0100

    Make dialog open and close when it is needed
    using two buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 console.log("Everything works as expected");
 
 const dialog = document.getElementsByTagName("dialog")[0];
+const addItem = document.getElementById("addItem");
+const closeDialog = document.getElementById("closeDialog");
 
-dialog.close();
+addItem.addEventListener('click', ()=> {
+    dialog.showModal();
+});
+
+closeDialog.addEventListener('click', ()=> {
+    dialog.close();
+});
