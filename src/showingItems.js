6f384bdb1b563c8eaf7dd1561b33e4631ad08985
vuckoviic <vuckovica64@gmail.com>
2024-02-24 15:57:42 +0100

    showingItems() works as expected

--- src/showingItems.js
+++ src/showingItems.js
@@ -1,3 +1,7 @@
+import { todoItems } from "./index.js";
+
+const content = document.getElementById("content");
+
 export function showingItems() {
     const showItem = document.createElement("div");
     const checkbox = document.createElement("input");
@@ -5,10 +9,12 @@ export function showingItems() {
     const showTitle = document.createElement("div");
     const showDescription = document.createElement("div");
     const showDueDate = document.createElement("div");
-    showTitle.innerHTML ="<h3>" + this.title + "</h3>";
-    showDescription.innerHTML ="<p>" + this.description + "</p>";
-    showDueDate.innerHTML ="<p>" + this.dueDate + "</p>";
+    showTitle.innerHTML ="<h3>" + todoItems[(todoItems.length-1)].title + "</h3>";
+    showDescription.innerHTML ="<p>" + todoItems[(todoItems.length-1)].description + "</p>";
+    showDueDate.innerHTML ="<p>" + todoItems[(todoItems.length-1)].dueDate + "</p>";
     
+    console.log(todoItems.length - 1);
+
     showItem.appendChild(checkbox);
     showItem.appendChild(showTitle);
     showItem.appendChild(showDescription);
