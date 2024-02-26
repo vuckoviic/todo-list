60ec65cedbd502f92ab0a92161ac089a8af8d71b
vuckoviic <vuckovica64@gmail.com>
2024-02-26 10:52:59 +0100

    Separate setting color by priority logic

---
+++ src/settingPriority.js
@@ -0,0 +1,16 @@
+import { priority } from "./createTodoItem.js";
+import { showItem, showingItems } from "./showingItems.js";
+
+export function settingPriority () {
+    if (priority === "low") {
+        showItem.style.backgroundColor = "lightblue";
+    }
+    
+    else if (priority == "normal") {
+        showItem.style.backgroundColor = "rgb(51, 126, 245)";
+    }
+    
+    else {
+        showItem.style.backgroundColor = "red";
+    }
+}
