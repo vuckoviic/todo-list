b054cb347ab91d89ec98025c2d510914699a4c0d
vuckoviic <vuckovica64@gmail.com>
2024-02-26 10:27:17 +0100

    Separate logic for checking checkbox and setting color by priority

--- src/showingItems.js
+++ src/showingItems.js
@@ -1,10 +1,14 @@
+import { checkCheckbox } from "./checkCheckbox.js";
 import { priority } from "./createTodoItem.js";
 import { todoItems } from "./index.js";
+import { settingPriority } from "./settingPriority.js";
 
 const content = document.getElementById("content");
 
+export let showItem;
+
 export function showingItems() {
-    const showItem = document.createElement("div");
+    showItem = document.createElement("div");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.classList.add("checkbox");
@@ -14,20 +18,8 @@ export function showingItems() {
     showTitle.innerHTML ="<h3>" + todoItems[(todoItems.length-1)].title + "</h3>";
     showDescription.innerHTML ="<p>" + todoItems[(todoItems.length-1)].description + "</p>";
     showDueDate.innerHTML ="<p>" + todoItems[(todoItems.length-1)].dueDate + "</p>";
-    
-    console.log(todoItems.length - 1);
 
-    if (priority === "low") {
-        showItem.style.backgroundColor = "lightblue";
-    }
-
-    else if (priority == "normal") {
-        showItem.style.backgroundColor = "rgb(51, 126, 245)";
-    }
-
-    else {
-        showItem.style.backgroundColor = "red";
-    }
+    settingPriority();
 
     showItem.appendChild(checkbox);
     showItem.appendChild(showTitle);
@@ -35,5 +27,8 @@ export function showingItems() {
     showItem.appendChild(showDueDate);
     showItem.classList.add("showItemDiv");
     showDescription.classList.add("showDescription");
+
     content.appendChild(showItem);
+
+    showItem.addEventListener('click', checkCheckbox);
 }
