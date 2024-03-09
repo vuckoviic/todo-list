9d8137a58119eb0731b235119b0334ae6c9c9636
vuckoviic <vuckovica64@gmail.com>
2024-03-09 20:44:16 +0100

    Separate projects logic into module for itself

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { showingItems } from "./showingItems.js";
 import { createTodoItemFunc } from  "./createTodoItem.js";
 import { clearContent } from "./clearContent.js";
+import { createNewProject } from "./projects.js";
 
 const dialog = document.getElementsByTagName("dialog")[0];
 export { dialog };
@@ -33,64 +34,7 @@ const createTodoItem = document.getElementById("createTodoItem");
 
 createTodoItem.addEventListener('click', createTodoItemFunc);
 
-const newProjectButton = document.getElementById("newProject");
-const projects = document.getElementById("projects");
+export const newProjectButton = document.getElementById("newProject");
+export const projects = document.getElementById("projects");
 
-newProjectButton.addEventListener("click", () => {
-    const project = document.createElement("button");
-    const projectNameInput = document.createElement("input");
-    const projectSave = document.createElement("button");
-    const projectExit = document.createElement("button");
-    
-    projectSave.innerText = "Save";
-    projectExit.innerText = "Exit";
-
-    project.style.padding = "3px";
-
-    projectSave.style.backgroundColor = "white";
-    projectSave.style.maxHeight = "15px";
-    projectSave.style.maxWidth = "15px";
-    projectSave.style.fontSize = "1em";
-    projectSave.style.color = "navy";
-    projectSave.style.marginTop = "5px";
-
-    projectExit.style.backgroundColor = "white";
-    projectExit.style.maxHeight = "15px";
-    projectExit.style.maxWidth = "15px";
-    projectExit.style.fontSize = "1em";
-    projectExit.style.color = "navy";
-    projectExit.style.marginTop = "5px";
-
-    project.appendChild(projectNameInput);
-    project.appendChild(projectExit);
-    project.appendChild(projectSave);
-
-    projectSave.addEventListener("click", () => {
-        
-        let name = projectNameInput.value;
-        
-        if (name.length < 1) {
-            alert("Please Insert Name For Your Project.");
-        }
-
-        else {
-            project.innerText = name;
-            name = new Array();
-    
-            project.addEventListener("click", ()=> {
-                clearContent();
-                // showingItems();
-            });
-        }
-
-    });
-
-    projectExit.addEventListener("click", () => {
-        projects.removeChild(project);
-    })
-
-    projects.removeChild(newProjectButton);
-    projects.appendChild(project);
-    projects.appendChild(newProjectButton);
-
-});
+newProjectButton.addEventListener("click", createNewProject)
---
+++ src/projects.js
@@ -0,0 +1,61 @@
+import { projects, newProjectButton } from "./index.js";
+import { clearContent } from "./clearContent.js";
+
+export function createNewProject() {
+    const project = document.createElement("button");
+    const projectNameInput = document.createElement("input");
+    const projectSave = document.createElement("button");
+    const projectExit = document.createElement("button");
+    
+    projectSave.innerText = "Save";
+    projectExit.innerText = "Exit";
+
+    project.style.padding = "3px";
+
+    projectSave.style.backgroundColor = "white";
+    projectSave.style.maxHeight = "15px";
+    projectSave.style.maxWidth = "15px";
+    projectSave.style.fontSize = "1em";
+    projectSave.style.color = "navy";
+    projectSave.style.marginTop = "5px";
+
+    projectExit.style.backgroundColor = "white";
+    projectExit.style.maxHeight = "15px";
+    projectExit.style.maxWidth = "15px";
+    projectExit.style.fontSize = "1em";
+    projectExit.style.color = "navy";
+    projectExit.style.marginTop = "5px";
+
+    project.appendChild(projectNameInput);
+    project.appendChild(projectExit);
+    project.appendChild(projectSave);
+
+    projectSave.addEventListener("click", () => {
+        
+        let name = projectNameInput.value;
+        
+        if (name.length < 1) {
+            alert("Please Insert Name For Your Project.");
+        }
+
+        else {
+            project.innerText = name;
+            name = new Array();
+    
+            project.addEventListener("click", ()=> {
+                clearContent();
+                // showingItems();
+            });
+        }
+
+    });
+
+    projectExit.addEventListener("click", () => {
+        projects.removeChild(project);
+    })
+
+    projects.removeChild(newProjectButton);
+    projects.appendChild(project);
+    projects.appendChild(newProjectButton);
+
+}
