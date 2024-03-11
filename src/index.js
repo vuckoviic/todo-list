e196de32a07edf65f0b8c8a6f1610ed15fc0ac81
vuckoviic <vuckovica64@gmail.com>
2024-03-11 18:23:52 +0100

    Remove and rename todoItems array
    it is now called inboxArray and it is located at projects.js module

--- src/index.js
+++ src/index.js
@@ -28,8 +28,6 @@ export class Item {
     };
 }
 
-export let todoItems = [];
-
 const createTodoItem = document.getElementById("createTodoItem");
 
 createTodoItem.addEventListener('click', createTodoItemFunc);
